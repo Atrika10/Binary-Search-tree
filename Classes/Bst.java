76da7dfda41da242ba6c0b82fe828a564be91008
ATRIKA SHOW <readeratrika08@gmail.com>
2023-09-13 11:57:28 +0530

    delete a node from BST

--- Classes/Bst.java
+++ Classes/Bst.java
@@ -1,9 +1,10 @@
-public class Bst{
-    public static class Node{
+public class Bst {
+    public static class Node {
         int value;
         Node left;
         Node right;
-        Node(int val){
+
+        Node(int val) {
             this.value = val;
             this.left = null;
             this.right = null;
@@ -11,27 +12,27 @@ public class Bst{
     }
 
     // insert function will return root for each values
-    public static Node insert(Node root, int val){
+    public static Node insert(Node root, int val) {
         // first case
         if (root == null) {
             // create new node
             Node newNode = new Node(val);
-            root = newNode;     // newNode is my root
+            root = newNode; // newNode is my root
             return root;
         }
 
-        //In other case
+        // In other case
         if (root.value > val) {
             // left subtree
             root.left = insert(root.left, val); // Whatever answer we'll get, it will be stored in root.left
-        }else{
+        } else {
             root.right = insert(root.right, val);
         }
         return root;
     }
 
     // print bst's value in inOrder fashion
-    public static void inOrder(Node root){      // left.data -> root.data -> right.data
+    public static void inOrder(Node root) { // left.data -> root.data -> right.data
         // base case
         if (root == null) {
             return;
@@ -42,7 +43,7 @@ public class Bst{
     }
 
     // Time Comp. => O(n)
-    public static void preOrder(Node root){
+    public static void preOrder(Node root) {
         // base case
         if (root == null) {
             return;
@@ -53,8 +54,8 @@ public class Bst{
     }
 
     // Time Comp. => O(n)
-    public static void postOrder(Node root){
-         // base case
+    public static void postOrder(Node root) {
+        // base case
         if (root == null) {
             return;
         }
@@ -66,23 +67,69 @@ public class Bst{
     // Search a key in BST
     // if it exist return true else return false
     // Time complexity will be O(H) where H is the height of the Binary Search Tree
-    public static boolean searchKeyInBST(Node root, int key){
+    public static boolean searchKeyInBST(Node root, int key) {
         // base case
         if (root == null) {
-            return false;       // in any case if I found root = null means I've searched all node & didn't get my key
+            return false; // in any case if I found root = null means I've searched all node & didn't get
+                          // my key
         }
         if (root.value == key) {
             return true;
-        }else if(key > root.value){
-            // I'll return what my right subtree return to me 
+        } else if (key > root.value) {
+            // I'll return what my right subtree return to me
             return searchKeyInBST(root.right, key);
-        }else{
-            return searchKeyInBST(root.left, key);  // if my key is less that root.value
+        } else {
+            return searchKeyInBST(root.left, key); // if my key is less that root.value
         }
     }
 
-    public static void main(String args[]){
-        int values[] = {5,4,6,1,2,8};
+    /* where "h" is the height of the tree.
+    In the worst case, the height of a BST can be O(n), where "n" is the number of nodes in the tree.
+    so the worst-case time complexity for deleting a node in a BST is O(n)
+    */
+    // we will return root node
+    public static Node deleteNode(Node root, int deleteNodeValue) {
+
+        if (root.value > deleteNodeValue) { // if value exist in the left side
+            root.left = deleteNode(root.left, deleteNodeValue);
+        } 
+        else if (root.value < deleteNodeValue) {
+            root.right= deleteNode(root.right, deleteNodeValue);// if value exist in the right side
+        } 
+        else { // if I've to delete the root node
+            // If I've to delete the root node, there will be 3 case
+            // case 1 => leaf node delete
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+            // case 2 => if it has 1 children
+            if (root.left == null) {
+                return root.right;  // if left node is null, will return right children to the parent node
+            }
+            if(root.right ==  null){
+                return root.left;
+            }
+
+            // case 3 => if it has both children
+            // we need to find inorder successor or inorder predecessor of that node which we've to delete
+            Node inSucc = inOrderSuccessor(root.right, deleteNodeValue);
+            root.value = inSucc.value;  // replace root value with IS value
+            root.right = deleteNode(root.right, inSucc.value);
+
+        }
+        return root;
+    }
+
+    public static Node inOrderSuccessor(Node root, int deleteNodeValue){
+        // I've to find left most node of right subtree
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+
+    public static void main(String args[]) {
+        int values[] = { 5, 4, 6, 1, 2, 8 };
         Node root = null;
 
         for (int i = 0; i < values.length; i++) {
@@ -90,10 +137,11 @@ public class Bst{
         }
 
         // print BST
-        postOrder(root);
-        System.out.println("null");     // 1-> 2-> 4-> 5-> 6-> 8-> null
-
-        //System.out.println(searchKeyInBST(root, 16));
-
+        inOrder(root);
+        System.out.println("null");
+        deleteNode(root, 5);
+        // print BST
+        inOrder(root);
+        System.out.println("null");
     }
 }
